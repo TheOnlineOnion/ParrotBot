abbed2f7b59ab027f1f183f367055e62e80f13f1
your_local_bookworm <126800201+TheOnlineOnion@users.noreply.github.com>
2025-01-20 10:35:43 +0100

    Create ParrotMain.py
    Dit is het hoofdbestand.

---
+++ ParrotMain.py
@@ -0,0 +1,6 @@
+def main():
+  #Using helper functions from other documents, put everything together so that the ParrotBot can function properly
+  #Helper functions for: 1 movement, 2 producing/recording sound, 3 recording with camera, 4 machine learning
+
+if __name__ == '__main__':
+  main()
