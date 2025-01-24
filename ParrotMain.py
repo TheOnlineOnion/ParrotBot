12c31a92cb4aa7f2f41f0d18bb892aebcc7997c8
your_local_bookworm <126800201+TheOnlineOnion@users.noreply.github.com>
2025-01-24 21:19:33 +0100

    Update ParrotMain.py

--- ParrotMain.py
+++ ParrotMain.py
@@ -1,6 +1,6 @@
 def main():
-  #Using helper functions from other documents, put everything together so that the ParrotBot can function properly
-  #Helper functions for: 1 movement, 2 producing/recording sound, 3 recording with camera, 4 machine learning
+  #Maak gebruik van de helper functies om alle functionaliteiten van de robot waar te maken
+  #Helper functies voor: 1 beweging, 2 geluid produceren/opnemen, 3 beeld opnemen met de camera, 4 machine learning
 
 if __name__ == '__main__':
   main()
