53e4723e798c056285dcb92c252f8770a989640f
your_local_bookworm <126800201+TheOnlineOnion@users.noreply.github.com>
2025-02-28 14:07:23 +0100

    Update SoundStatistics.py

--- SoundStatistics.py
+++ SoundStatistics.py
@@ -12,7 +12,15 @@ print(features[0].shape, features[1].shape)
 f1 = np.array([features[0][:, fn.index('spectral_centroid_mean')], features[0][:, fn.index('energy_entropy_mean')]])
 f2 = np.array([features[1][:, fn.index('spectral_centroid_mean')], features[1][:, fn.index('energy_entropy_mean')]])
 
-x = f1[0, :]
-y = f1[0, :]
-plt.plot(x)
+x1 = f1[0, :]
+y1 = f1[1, :]
+x2 = f2[0, :]
+y2 = f2[1, :]
+
+plt.scatter(x1, y1, label="alarm")
+plt.scatter(x2, y2, label="crickets")
+plt.xlabel('spectral_centroid_mean')
+plt.ylabel('energy_entropy_mean')
+
+plt.legend()
 plt.show()
